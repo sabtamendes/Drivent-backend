915464c543b1a73e1b108f4473c49ee1c3406338
Sabta Mendes <alencarsabta@gmail.com>
2023-02-06 22:38:49 -0400

    first config

---
+++ tests/factories/index.ts
@@ -0,0 +1,7 @@
+export * from "./users-factory";
+export * from "./events-factory";
+export * from "./sessions-factory";
+export * from "./enrollments-factory";
+export * from "./tickets-factory";
+export * from "./payments-factory";
+export * from "./hotels-factory";
